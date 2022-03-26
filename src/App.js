08113a4b9d6d28f41c25ed55a4c25b5d63f38918
commanderdestro <67872394+commanderdestro@users.noreply.github.com>
2022-03-26 15:04:53 -0500

    forge logo and coming soon css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import forgeLogo from './images/forgeNoBorder.png';
+import {ReactComponent as LinkedIn} from './images/linkedin.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='middle-content'>
+        <img className='logo-forge' id='forgeLogo' src={forgeLogo} alt="Energy Forge" />
+        <span className='typewritter-text'>More information coming soon...   </span>
+      </div>
+      <div className='footer'>
+        <LinkedIn className='logo-bottom' />
+      </div>
     </div>
   );
 }
